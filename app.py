88102421825e3b54819b6ea060ac72cb1796cf28
apex1000 <siddhartsingh9876@gmail.com>
2019-04-20 09:39:06 +0530

    Ready to setup

--- app.py
+++ app.py
@@ -55,5 +55,4 @@ def pointmap():
     return render_template('./roadtraffic/index.html')
 
 if __name__ == '__main__':
-    app.secret_key='secret123'
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
